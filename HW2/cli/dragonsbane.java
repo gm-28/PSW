4e3f9b703eabf5b5f78c72de9c00bdf6650f0ac4
Guilherme Moreira <93450195+gm-28@users.noreply.github.com>
2021-11-27 01:29:58 +0000

    Add files via upload

--- HW2/cli/dragonsbane.java
+++ HW2/cli/dragonsbane.java
@@ -5,32 +5,43 @@ import logic.heroi;
 import logic.objeto;
 import logic.jogo;
 import logic.tabuleiro;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class dragonsbane {
+public class dragonsbane{
     public static void main(String[] args) {
-
         tabuleiro tab = new tabuleiro();
-        heroi heroi = new heroi();
-        dragao dragao = new dragao();
-        objeto chave = new objeto();
+        heroi heroi = new heroi(1,1,true);
+        dragao dragao = new dragao( 3,1,true);
+        //ArrayList<dragao> dragoes;
+        objeto espada = new objeto(8,1,true);
         Scanner myObj = new Scanner(System.in);
         char movimento;
-        System.out.println("Here be dragons");
-        System.out.println("Usa [WASD] para guiar o heroi até à saida sem que o dragão te apanhe!");
-        System.out.println(" ");
-        jogo dragonsbane = new jogo();
 
-        dragonsbane.pos_inicial(heroi, dragao, chave);
-        dragonsbane.atualiza_pos(tab, heroi, dragao, chave);
+        System.out.println("Here be dragons");
+        System.out.println("Usa [WASD] para guiar o heroi até à saida sem que os dragões te apanhem!");
+        System.out.println(" ");
+        System.out.print("Quantos dragões vai usar? ");
+
+//        int n_drag;
+//        while(true){
+//            System.out.println("Até 3 dragões!");
+//            n_drag= myObj.nextInt();
+//            if(n_drag<=3)break;
+//        }
+        //dragoes = new ArrayList<>(n_drag);
+
+        jogo dragonsbane = new jogo(tab, heroi,dragao, espada);
+
+
+        System.out.println("--------------------");
         print_tab(tab);
+        System.out.println("--------------------");
 
         while (true) {
             movimento = myObj.next().charAt(0);
-            //movimento heroi e atualizacao/verficaçao do estado
-            heroi.move_heroi(movimento, tab, heroi,dragao,chave);
-            dragonsbane.atualiza_pos(tab, heroi, dragao, chave);
-            dragonsbane.estado(tab,heroi,dragao);
+            dragonsbane.atualizacao(movimento,tab,heroi,dragao,espada);
 
             if(dragonsbane.vitoria) {
                 System.out.println("PARABÉNS!!");
@@ -40,30 +51,17 @@ public class dragonsbane {
             if (!heroi.presente){
                 System.out.println("FOSTE COMIDO");
                 System.out.println("[TENTA OUTRA VEZ]");
+                System.out.println("--------------------");
+                print_tab(tab);
+                System.out.println("--------------------");
                 break;
             }
-
-            //movimento dragao e atualizacao/verficaçao do estado
-            dragao.dragao_dir(tab,dragao,heroi);
-            dragonsbane.atualiza_pos(tab, heroi, dragao, chave);
-            dragonsbane.estado(tab,heroi,dragao);
-
-            if(dragonsbane.vitoria) {
-                System.out.println("PARABÉNS!!");
-                System.out.println("[FIM]");
-                break;
-            }
-            if (!heroi.presente){
-                System.out.println("FOSTE COMIDO");
-                System.out.println("[TENTA OUTRA VEZ]");
-                break;
-            }
-
+            System.out.println("--------------------");
             print_tab(tab);
+            System.out.println("--------------------");
             System.out.println(" ");
         }
     }
-
 
    private static void print_tab(tabuleiro tab) {
         for (int i = 0; i < tab.tabuleiro.length; i++) {
@@ -74,4 +72,5 @@ public class dragonsbane {
             System.out.println(' ');
         }
     }
+
 }
